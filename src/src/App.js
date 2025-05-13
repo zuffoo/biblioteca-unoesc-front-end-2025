e6fe246d05852b6a2634c2fd5dc89c5cfc3bfd1a
Ricardo Zuffo <rzuffo11@gmail.com>
2025-05-13 20:01:41 -0300

    Integração com bootstrap

---
+++ src/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Biblioteca 2025</h1>
+      <buttom className='btn btn-primary'>Teste</buttom>
+    </>
+  );
+}
+
+export default App;
